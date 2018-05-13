e7fd628f16690a3ea78598489d2b23c220ca0015
fidelm02 <fidelm02@gmail.com>
2018-05-12 21:21:17 -0500

    Ya esta vestido el ninio (UI)

---
+++ Broncos_ChopShop/Assets/Scenes/Scripts/GraphicsScripts/KidModeUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KidModeUI : MonoBehaviour {
+    Toggle configKid;
+	// Use this for initialization
+	void Start () {
+        bool KidM = PlayerPrefs.GetInt("KidMode") == 1;
+        this.gameObject.GetComponent<Toggle>().isOn = configKid;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UpdateKidMode()
+    {
+        bool KiDM = this.gameObject.GetComponent<Toggle>().isOn;
+        int i = 0;
+        if (KiDM)
+            i = 1;
+        PlayerPrefs.SetInt("KidMode", i);
+    }
+}
